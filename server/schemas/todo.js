81011ca600ab08b8feca9c0b7f9e0510f2b451b4
Tödter <hto@iks-gmbh.com>
2016-11-17 15:31:05 +0100

    add a simple command line interface

---
+++ server/schemas/todo.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TodoSchema = new Schema({
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: 0
+    }
+});
+
+var Todo = mongoose.model('Todo', TodoSchema);
+
+module.exports.Todo = Todo;
---
+++ server/schemas/user.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => /^[a-z0-9]([a-z0-9.]+[a-z0-9])?\@[a-z0-9.-]+$/.test(value),
+            message: '{VALUE} is not a valid email address!'
+        },
+        minlength: 3,
+        trim: true
+    }
+})
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports.User = User;
